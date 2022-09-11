a6357271ba532fa02b5ed83846de5bd453c1f826
ShalaevRoman <mc.shel24@gmail.com>
2022-09-11 18:50:08 +0300

    change publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/TEST_TASK/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/ShalaevRoman_test_task/' : '/',
   transpileDependencies: [
     'vuetify'
   ]
